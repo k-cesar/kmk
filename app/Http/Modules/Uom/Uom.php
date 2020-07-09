b3cdac62a9f83adaf493e4f12c6a8bfdd4c6d8ef
Stalin Sánchez <ssanchez684@gmail.com>
2020-07-09 22:04:44 +0000

    Se agregó SoftDeletes a Brands, Providers y Uoms

--- app/Http/Modules/Uom/Uom.php
+++ app/Http/Modules/Uom/Uom.php
@@ -4,10 +4,11 @@ namespace App\Http\Modules\Uom;
 
 use App\Traits\SecureDeletes;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Uom extends Model
 {
-    use SecureDeletes;
+    use SoftDeletes, SecureDeletes;
 
     /**
      * The attributes that are mass assignable.
