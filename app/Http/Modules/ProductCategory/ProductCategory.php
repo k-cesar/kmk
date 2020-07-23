1ab857997a14e804ff63f2890ca54a625db4ee49
userlab-nelsontorres <ntorres144@gmail.com>
2020-07-23 09:15:13 -0600

    Nt/product category (#23)
    * Product Category crud
    
    * add filtering to list
    
    * alloow to update only name
    
    * fix rule get

---
+++ app/Http/Modules/ProductCategory/ProductCategory.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Modules\ProductCategory;
+
+use App\Traits\SecureDeletes;
+use App\Http\Modules\ProductDepartment\ProductDepartment;
+use Illuminate\Database\Eloquent\Model;
+
+class ProductCategory extends Model
+{
+    use SecureDeletes;
+    
+    /** 
+     * Table Associated with the model
+     */ 
+    protected $table = 'product_categories';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'product_department_id'
+    ];
+   
+    /**
+     * The relations to eager load on every query.
+     *
+     * @var array
+     */
+    protected $with = ['productDepartment'];
+
+     /**
+     * Get the Product department that owns the product category.
+     * 
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function productDepartment()
+    {
+        return $this->belongsTo(ProductDepartment::class);
+    }
+
+}
---
+++ app/Http/Modules/ProductCategory/ProductCategoryController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Modules\ProductCategory;
+
+use App\Http\Controllers\Controller;
+use App\Http\Modules\ProductCategory\ProductCategory;
+
+class ProductCategoryController extends Controller
+{
+  /**
+   * Display a listing of the resource.
+   *
+   * @return \Illuminate\Http\JsonResponse
+   */
+  public function index(ProductCategoryRequest $request)
+  {    
+    $where = [];
+    if ($request->get('name')) {
+      array_push($where, ['name', 'ilike', '%'.$request->get('name').'%']);
+    } 
+    if ($request->get('product_department_id')) {
+      array_push($where, ['product_department_id', '=', $request->get('product_department_id')]);
+    }
+
+    $productCategory = ProductCategory::where($where)->paginate();
+    
+   
+    return $this->showAll($productCategory);
+  }
+
+  /**
+   * Store a newly created resource in storage.
+   *
+   * @param  App\Http\Modules\ProductCategory\ProductCategoryRequest  $request
+   * @return \Illuminate\Http\JsonResponse
+   */
+  public function store(ProductCategoryRequest $request)
+  {
+    $productCategory = ProductCategory::create($request->validated());
+
+    return $this->showOne($productCategory, 201);
+  }
+
+  /**
+   * Display the specified resource.
+   *
+   * @param  App\Http\Modules\ProductCategory\ProductCategory  $productCategory
+   * @return \Illuminate\Http\JsonResponse
+   */
+  public function show(ProductCategory $productCategory)
+  {
+    return $this->showOne($productCategory);
+  }
+
+  /**
+   * Update the specified resource in storage.
+   *
+   * @param  App\Http\Modules\ProductCategory\ProductCategoryRequest  $request
+   * @param  App\Http\Modules\ProductCategory\ProductCategory  $productCategory
+   * @return \Illuminate\Http\JsonResponse
+   */
+  public function update(ProductCategoryRequest $request, ProductCategory $productCategory)
+  {    
+
+    $productCategory->update($request->validated());
+
+    return $this->showOne($productCategory);
+  }
+
+  /**
+   * Remove the specified resource from storage.
+   *
+   * @param  App\Http\Modules\ProductCategory\ProductCategory  $productCategory
+   * @return \Illuminate\Http\JsonResponse
+   */
+  public function destroy(ProductCategory $productCategory)
+  {
+    $productCategory->secureDelete();
+
+    return $this->showOne($productCategory);
+  }
+}
