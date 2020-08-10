57ef3bd8ca20f669e55c37c75e22cce5e6e2a1ad
kemok-stalin <66266522+kemok-stalin@users.noreply.github.com>
2020-08-10 15:04:00 -0400

    Se restauró el Servicio para listar los permisos (#50)
    Co-authored-by: Stalin Sánchez <ssanchez684@gmail.com>

---
+++ app/Http/Modules/Permission/PermissionController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Modules\Permission;
+
+use App\Support\Helper;
+use App\Traits\ApiResponser;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Schema;
+use Spatie\Permission\Models\Permission;
+
+class PermissionController extends Controller
+{
+  use ApiResponser;
+
+  /**
+   * Display a listing of the resource.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function index()
+  {
+    $permissions = Permission::query();
+
+    return $this->showAll($permissions, Schema::getColumnListing((new Permission)->getTable()));
+  }
+}
