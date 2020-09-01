ef4bcb9f2317704a4e5792cfa279672fda0e80f5
Nelson Rodriguez <nelsonmatul2013@gmail.com>
2020-09-01 15:16:27 -0600

    *FEAT* se agrega el test para store-adjustment y correccion de permisos

--- tests/Feature/StoreAdjustmentControllerTest.php
+++ tests/Feature/StoreAdjustmentControllerTest.php
@@ -2,21 +2,49 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\ApiTestCase;
+use App\Http\Modules\Store\Store;
 use Illuminate\Foundation\Testing\WithFaker;
-use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 
-class StoreAdjustmentControllerTest extends TestCase
+class StoreAdjustmentControllerTest extends ApiTestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function testExample()
-    {
-        $response = $this->get('/');
+    use DatabaseMigrations, RefreshDatabase;
 
-        $response->assertStatus(200);
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->seed(['PermissionSeeder', 'RoleSeeder', 'UserSeeder', 'StoreSeeder', 'ProductSeeder', 'PurchaseSeeder']);
+    }
+
+    /**
+     * @test
+     */
+    public function a_guest_cannot_access_to_store_adjustment_resources()
+    {
+        $this->getJson(route('store-adjustment.index'))->assertUnauthorized();
+    }
+
+    /**
+     * @test
+     */
+    public function an_user_without_permission_cannot_access_to_store_adjustment_resources()
+    {
+        $this->signIn();
+        $this->getJson(route('store-adjustment.index'))->assertForbidden();
+    }
+
+    /**
+     * @test
+     */
+    public function an_user_with_permission_can_see_all_store_adjustment()
+    {
+        $this->signInWithPermissionsTo(['store-adjustment.index']);
+
+        $store = Store::has('products')->first();
+
+        $this->getJson(route('store-adjustment.index', ['store_id' => $store->id]))
+        ->assertOk();
     }
 }
